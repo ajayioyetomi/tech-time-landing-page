65852eec39978c3b1610ef0435524ae83ed6ac28
ajayi oyetomi <ajayioyetomi@gmail.com>
2023-03-27 16:17:34 +0100

    added responsiveness for the header file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import {HiMenu as MenuIcon} from 'react-icons/hi';
+import {FaTimes as CancelIcon} from 'react-icons/fa';
 import { useLocation } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const linkList = [
   {path:'/',name:'Home'},
@@ -16,7 +17,9 @@ const linkList = [
 
 const Header = () => {
   const location = useLocation();
+  const [activeMenu,setActiveMenu] = useState(false);
   const [activeIndex,setActiveIndex] = useState<number>(0);
+  const [isTop,setIsTop] = useState<boolean>(true);
   const handleActiveLink = () =>{
       let path = location.pathname;
       let i,len = linkList.length;
@@ -29,25 +32,40 @@ const Header = () => {
           break;
         }
       }
-
   }
   useEffect(()=>{
     handleActiveLink()
   },[location.pathname])
+  useEffect(()=>{
+    const handleScroll = () =>{
+      let yScroll = window.scrollY;
+      if(yScroll > 5){
+        setIsTop(false);
+      }
+      else{
+        setIsTop(true);
+      }
+    }
+    handleScroll();
+    window.addEventListener('scroll',handleScroll);
+    return ()=> window.removeEventListener('scroll',handleScroll); 
+  },[])
   return (
     <HeaderWrapper>
-      <div>
+      <div style={{height:isTop?'110px':'80px'}}>
         <StyledLink to="/">
-          <img src={logo} alt="logo" />
+          <img style={{width:isTop?'120px':'100px'}} src={logo} alt="logo" />
         </StyledLink>
         <NavWrapper>
-          <div>
+          <div className={activeMenu ? 'active':''} >
+            <MenuButton onClick={()=>setActiveMenu(false)} style={{color:'var(--primary-color)',alignSelf:'flex-end'}}>
+                <CancelIcon/>
+            </MenuButton>
             {linkList.map((eLink:any,index:number) =>
               <StyledLink className={activeIndex == index?'active':''} key={index} to={eLink.path}>{eLink.name}</StyledLink>
             )}
-
           </div>
-          <MenuButton>
+          <MenuButton onClick={()=>setActiveMenu(true)}>
               <MenuIcon/>
           </MenuButton>
         </NavWrapper>
@@ -65,25 +83,29 @@ const HeaderWrapper = styled.header`
   display:flex;
   align-items:center;
   justify-content:center;
-  position:sticky;
+  position:fixed;
+  z-index:100;
+  width:100%;
   top:0;
   left:0;
-  background-color:var(--primary-background);
+  background:transparent;
   & > div{
     width:var(--main-width);
-    height:100px;
+    height:110px;
     display:flex;
     align-items:center;
     justify-content:space-between;
+    transition:all 0.4s;
   }
   & > div > a{
-    width:100px;
+    width:120px;
     display:flex;
     justify-content:center;
     align-item:center;
   }
   & > div > a > img{
-    width:100px;
+    width:120px;
+    transition:all 0.4s;
   }
 `;
 
@@ -95,12 +117,38 @@ const NavWrapper = styled.nav`
     display:flex;
     align-items:center;
     gap:25px;
+    position:relative;
+    right:0;
    }
    & > div > a:last-of-type{
       padding:10px 25px;
       border-radius:4px;
       color:var(--primary-color) !important;
       background-color:var(--white-color);
+   }
+   @media screen and (max-width:810px){
+      & > div{
+        position:fixed;
+        flex-direction:column;
+        align-items:flex-start;
+        gap:20px;
+        width:320px;
+        top:0;
+        right:-320px;
+        background-color:var(--dark-background);
+        height:100%;
+        padding:20px;
+        transition:all 0.6s;
+      }
+      & > div.active{
+        right:0px;
+      }
+      & > div > a:last-of-type{
+        padding:10px 25px;
+        border-radius:4px;
+        color:var(--white-color) !important;
+        background-color:var(--primary-color);
+     }
    }
 `;
 
@@ -111,6 +159,14 @@ const StyledLink = styled(Link)`
    }
    &.active{
     color:var(--white-color);
+   }
+   @media screen and (max-width : 810px){
+    &:hover{
+      color: var(--primary-color);
+     }
+     &.active{
+      color:var(--primary-color);
+     }
    }
 `
 
@@ -123,10 +179,12 @@ const MenuButton = styled.button`
    justify-content:center;
    align-items:center;
    font-size:25px;
-   @media screen and (max-width:767px){
+   cursor:pointer;
+   @media screen and (max-width:810px){
     display:flex;
    }
 `
+
 
 
 
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components'
+import pattern from '../assets/pattern.png';
 
 const Home = () => {
   return (
     <main>
       <BannerWrapper>
-
+        <img src={pattern}  alt="background-pattern"/>
       </BannerWrapper>
     </main>
   )
@@ -18,8 +19,14 @@ const BannerWrapper = styled.div`
   justify-content:center;
   align-items:center;
   overflow:hidden;
+  height:100vh;
   background-color:var(--primary-background);
+  position:relative;
   & > img{
-
+    width:100%;
+    height:auto;
+    position:absolute;
+    top:0;
+    left:0%;
   }
 `
